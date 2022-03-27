7a477851cce8fbb94b8c1344d3f7cc9f9c338d28
Filsonprg <76943590+Filsonprg@users.noreply.github.com>
2022-03-27 19:02:20 +0200

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,18 +1,18 @@
 /* vložení jména, věku a narození pro Filip */
-INSERT INTO demo (jmeno, vek, narozeni) VALUES ("Filip", 17, 2005-02-01);
+INSERT INTO udaje (jmeno, vek, narozeni) VALUES ("Filip", 17, 2005-02-01);
 /* vložení jména, věku a narození pro Pepu */
-INSERT INTO demo (jmeno, vek, narozeni) VALUES ("Pepa", 17, 2004-09-22);
+INSERT INTO udaje (jmeno, vek, narozeni) VALUES ("Pepa", 17, 2004-09-22);
 /* updatování jména */
-UPDATE demo SET jmeno = "Pavel" WHERE id = 1;
+UPDATE udaje SET jmeno = "Pavel" WHERE id = 1;
 /* Smazání záznamu s id 2 */
-DELETE FROM demo WHERE id = 2;
+DELETE FROM udaje WHERE id = 2;
 /* Vybrání všeho z tabulky demo */
-SELECT * FROM demo
+SELECT * FROM udaje
 /* Vybrání jména a věku z tabulky demo */
-SELECT jmeno, vek FROM demo
+SELECT jmeno, vek FROM udaje
 /* Vybrání jména a věku kde id je rovno 1 nebo 2 */
-SELECT jmeno, vek FROM demo WHERE id = 1 OR 2
-/* Vybrání jména a věku z tabulky demo, a seřazené podle jmána */
-SELECT jmeno, vek FROM demo ORDER BY jmeno
-/* Vybrání jména a věku z tabulky demo seřazeno podle jmána a vypsané pouze první 3 entity */
-SELECT jmeno, vek FROM demo ORDER BY jmeno LIMIT 3
+SELECT jmeno, vek FROM udaje WHERE id = 1 OR 2
+/* Vybrání jména a věku z tabulky demo, a seřazené podle jména */
+SELECT jmeno, vek FROM udaje ORDER BY jmeno
+/* Vybrání jména a věku z tabulky demo seřazeno podle jména a vypsané pouze první 3 entity */
+SELECT jmeno, vek FROM udaje ORDER BY jmeno LIMIT 3
