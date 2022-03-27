770d5189f90e7a2b3b5089205a5c092bc940f90c
Filsonprg <76943590+Filsonprg@users.noreply.github.com>
2022-03-27 18:59:58 +0200

    Create ddl.sql

---
+++ ddl.sql
@@ -0,0 +1,11 @@
+/* vytvořit tabulku */
+CREATE TABLE demo (
+    /* atribut id */
+    id int UNSIGNED NOT NULL AUTO_INCREMENT,
+    /* atribut jméno */
+    jmeno varchar(35) NOT NULL,
+    /* atribut věk */
+    vek tinyint NOT NULL UNSIGNED,
+    /* atribut narozené */
+    narozeni date NOT NULL,
+);
